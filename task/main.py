79f47d78273b121e21bd24702f7c3511ede48891
SmileyFace799 <Henrikgran85@gmail.com>
2022-11-14 14:18:32 +0100

    stuff

--- task/main.py
+++ task/main.py
@@ -19,7 +19,7 @@ right_motor = Motor(Port.B)
 crane = Motor(Port.C)
 
 # Write your program here.
-robot = DriveBase(left_motor, right_motor, wheel_diameter=52.25, axle_track=140)
+robot = DriveBase(left_motor, right_motor, wheel_diameter=54, axle_track=140)
 
 #crane.run_time(-1000,1000)
 
@@ -33,16 +33,22 @@ def windmillRun():
     robot.straight(-360)
     
 def car():
-    #crane.run_angle(1000, 360, wait=True)
-    robot.turn(90)
-    #robot.straight(100)
-
+    robot.straight(850)
+    crane.run_angle(500, -1800, wait=True)
+    crane.run_angle(500, 1800, wait=True)
+    robot.straight(-850)
 
 while True:
     pressed = ev3.buttons.pressed()
-    if pressed:
+    if Button.RIGHT in pressed:
         windmillRun()
 
-    if Button.LEFT in ev3.buttons.pressed():
+    elif Button.LEFT in pressed:
         car()
+    
+    elif Button.UP in pressed:
+        crane.run_angle(1000, -5, wait=False)
+    
+    elif Button.DOWN in pressed:
+        crane.run_angle(1000, 5, wait=False)
     
